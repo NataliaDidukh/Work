a4cbfc1b8796efd396fc62ef7078502b0eb5eeb4
NataliaDidukh <naftusia.88@gmail.com>
2023-03-10 21:21:37 +0200

    Test

---
+++ While.py
@@ -0,0 +1,13 @@
+def comparison(a, b, c):
+    while True:
+        if a < b:
+            a += c
+        elif a >= b:
+            print("Have a nice day")
+            break
+
+
+comparison(5, 100, 2)
+
+
+
